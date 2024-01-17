66cd9aa5cbc5138052dfd843b69eecff7d168ec2
Jacob Metreveli <jacob.metreveli@gmail.com>
2024-01-17 13:10:15 +0200

    lala

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+
+
+int main() {
+    // Window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "G1", sf::Style::Titlebar | sf::Style::Close);
+    sf::Event ev;
+
+    // Game loop
+    while(window.isOpen()) {
+        // Event polling
+        while(window.pollEvent(ev)) {
+            switch (ev.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::KeyPressed:
+                    if(ev.key.code == sf::Keyboard::Escape)
+                        window.close();
+                    break;
+            }
+        }
+
+        // Update
+
+
+        // Render
+        window.clear(sf::Color::Red); // Clear old frame
+
+            // Draw the frame
+        window.display(); // Tell app that window is done drawing
+    }
+
+    return 0;
+}
