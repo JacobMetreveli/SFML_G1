75b07c2c648c46ab0b8e6530adc83f0561ae5b82
Jacob Metreveli <jacob.metreveli@gmail.com>
2024-01-18 10:26:06 +0200

    Updated Game.h and Game.cpp files

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,4 +1,4 @@
-#include "Game.h"
+#include "../Headers/Game.h"
 
 //Private functions
 void Game::initVariables() {
@@ -19,11 +19,37 @@ Game::~Game() {
     delete this->window;
 }
 
-// Functions
-void Game::update() {
+// Accessors
+const bool Game::running() const {
+    return this->window->isOpen();
+}
 
+// Functions
+void Game::pollEvents() {
+    // Event polling
+    while (this->window->pollEvent(this->ev)) {
+        switch (this->ev.type) {
+            case sf::Event::Closed:
+                this->window->close();
+                break;
+            case sf::Event::KeyPressed:
+                if (this->ev.key.code == sf::Keyboard::Escape)
+                    this->window->close();
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+
+void Game::update() {
+    this->pollEvents();
 }
 
 void Game::render() {
+    this->window->clear(sf::Color::Red);
 
+    // Draw game objects
+    this->window->display();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,19 @@
 #include <iostream>
-#include "../include/Game.h"
+#include "../Headers/Game.h"
+
 
 
 int main() {
-    // Window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "G1", sf::Style::Titlebar | sf::Style::Close);
-    sf::Event ev;
+    // Init game engine
+    Game game = Game();
 
     // Game loop
-    while(window.isOpen()) {
-        // Event polling
-        while(window.pollEvent(ev)) {
-            switch (ev.type) {
-                case sf::Event::Closed:
-                    window.close();
-                    break;
-                case sf::Event::KeyPressed:
-                    if(ev.key.code == sf::Keyboard::Escape)
-                        window.close();
-                    break;
-                default:
-                    break;
-            }
-        }
-
+    while(game.running()) {
         // Update
-
+        game.update();
 
         // Render
-        window.clear(sf::Color::Red); // Clear old frame
-
-            // Draw the frame
-        window.display(); // Tell app that window is done drawing
+        game.render();
     }
 
     return 0;
